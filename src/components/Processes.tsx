95f631b57058d96d3cba7fd904991c46a462a8e5
Hafeez <hafeezabubakar15@gmail.com>
2024-12-13 09:13:53 +0100

    Fixed

--- src/components/Processes.tsx
+++ src/components/Processes.tsx
@@ -5,8 +5,8 @@ const ProcessSection = () => {
   return (
     <div className="bg-primary">
       <SectionHeader/>
-      <div className="grid grid-cols-3 gap-8 px-8">
-        <div className="col-span-2 container mx-44">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 md:px-8">
+        <div className="md:col-span-2 container md:mx-44">
           <Services />
         </div>
       </div>
