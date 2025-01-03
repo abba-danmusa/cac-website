dc9857e232c8dc64a417c422f7f0ca2c253029b3
Hafeez <hafeezabubakar15@gmail.com>
2025-01-03 14:00:59 +0100

    servies-update

--- src/assests/index.js
+++ src/assests/index.js
@@ -11,6 +11,18 @@ import ellipse from './Ellipse 5.png'
 import home from './Home.svg'
 import map from './map.svg'
 import rg from './rg1.png'
+import tools1 from './tools1.svg'
+import tools2 from './tools2.svg'
+
+import hand1 from './hand1.svg'
+import hand2 from './hand2.svg'
+import build1 from './build1.svg'
+import build2 from './biuld2.svg'
+import people1 from './people1.svg'
+import people2 from './people2.svg'
+
+import person1 from './person1.svg'
+import person2 from './person2.svg'
 
 export {
     clip,
@@ -24,5 +36,11 @@ export {
     youtube,
     ellipse,
     home,
-    map, rg
+    map, rg, tools1, hand1, hand2, tools2,
+    build1,
+    build2,
+    people1,
+    people2,
+    person1,
+    person2,
 }
--- src/components/ProcessHeader.tsx
+++ src/components/ProcessHeader.tsx
@@ -1,7 +1,7 @@
 const SectionHeader = () => {
   return (
-    <div className="md:py-8 relative z-10 mx-8 md:mx-24">
-      <h2 className="text-2xl md:text-3xl font-sans font-bold text-black">
+    <div className="md:pt-8 relative z-10 mx-8 md:mx-24">
+      <h2 className="text-2xl leading-10 md:text-[40px] font-sans font-bold text-black">
         Let&apos;s Guide You {" "}
         <br  className="hidden md:block"/>
         Through <br  className="md:hidden block"/> The {" "}
